1f7101b839b92687f66b399189feb02488d96c1f
yforyuri <yforyurirho@gmail.com>
2021-08-13 09:37:22 +0900

    picktype & api property

--- backend/src/todo/entities/todo.entity.ts
+++ backend/src/todo/entities/todo.entity.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsString } from "class-validator";
 import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
@@ -8,10 +9,18 @@ export class Todo {
     id: number;
 
     @IsString()
+    @ApiProperty({
+        example: 'Eat',
+        description: '투두리스트의 제목'
+    })
     @Column('varchar')
     title: string;
 
     @IsString()
+    @ApiProperty({
+        example: 'get energy',
+        description: '투두리스트의 실횅'
+    })
     @Column('varchar')
     desc: string;
 
--- backend/src/todo/todo.controller.ts
+++ backend/src/todo/todo.controller.ts
@@ -1,7 +1,9 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { CreateTodoDto } from './dtos/createTodo.dto';
 import { TodoService } from './todo.service';
 
+@ApiTags('todo')
 @Controller('todo')
 export class TodoController {
     constructor(private readonly todoServices: TodoService) {}
