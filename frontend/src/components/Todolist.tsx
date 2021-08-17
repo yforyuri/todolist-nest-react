5824ae2b0a736f604aad35d4615e860b222e02b4
yforyuri <yforyurirho@gmail.com>
2021-08-17 10:30:18 +0900

    add swr

--- frontend/src/components/Todolist.tsx
+++ frontend/src/components/Todolist.tsx
@@ -1,6 +1,8 @@
 import React, { useState, FC, useEffect } from 'react';
 import axios from 'axios';
+import useSWR from 'swr';
 import AddTodo from './AddTodo';
+import { async } from 'q';
 
 interface Itodo {
   id: number;
@@ -12,30 +14,28 @@ interface Itodo {
 }
 
 const Todolist: FC = () => {
-  const [todos, setTodos] = useState<Itodo[]>([]);
+  const fetcher = async (url: string) => {
+    try {
+      const response = await axios.get(url);
 
-  useEffect(() => {
-    const getTodos = async () => {
-      try {
-        const response = await axios.get(
-          `${process.env.REACT_APP_BACK_URL}/todo`,
-        );
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-        if (response.statusText == 'OK') {
-          setTodos(response.data);
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    };
+  const { data, error } = useSWR<Itodo[]>(
+    `${process.env.REACT_APP_BACK_URL}/todo`,
+    fetcher,
+  );
 
-    getTodos();
-  }, []);
+  if (!data) return <div>Loading...</div>;
+  if (error) return <div>error</div>;
 
   return (
     <div>
       <AddTodo />
-      {todos.map((todo) => {
+      {data.map((todo) => {
         return (
           <li key={todo.id}>
             {todo.id} - {todo.title} - {todo.desc}
